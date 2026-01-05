a92c95506bf644c0c284f03b1cc9f772bba35f2a
xuyongjin <xuyongjin@zj-caiyu.com>
2026-01-05 18:38:12 +0800

    refactor state

---
+++ FreeTools/composeApp/src/jvmMain/kotlin/org/eu/freex/tools/modules/image/domain/model/IModel.kt
@@ -0,0 +1,4 @@
+package org.eu.freex.tools.modules.image.domain.model
+
+interface IModel {
+}
---
+++ FreeTools/composeApp/src/jvmMain/kotlin/org/eu/freex/tools/modules/image/presentation/core/StateExtensions.kt
@@ -0,0 +1,4 @@
+package org.eu.freex.tools.modules.image.presentation.core
+
+class StateExtensions {
+}
